3afa4305f231a54c34a0360088765ef24e898ee3
Autumn <a7autumn@outlook.com>
2024-11-19 01:56:01 +0800

    内容提交

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ServerHelp
 桦秋服务器帮助手册
+
+点击链接来进行访问
+[文档网站](https://aermoon-7.github.io/ServerHelp/)
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: '服务器帮助手册',
+    description: '为解决您在服务器的各类问题',
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '更新日志', link: '/changelog/' },
+            { text: '命令帮助', link: '/commandhelp/' },
+        ],
+        sidebar: 'auto',
+        lastUpdated: 'Last Updated',
+    }
+}
