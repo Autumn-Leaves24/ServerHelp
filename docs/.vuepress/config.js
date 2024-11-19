76e4302af83cbd79923e36ca1873705b4f4a7806
Autumn <a7autumn@outlook.com>
2024-11-19 19:12:17 +0800

    内容更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,11 +4,27 @@ module.exports = {
     base: '/ServerHelp/',
     themeConfig: {
         nav: [
-            { text: '主页', link: '/' },
+            { text: '首页', link: '/' },
             { text: '更新日志', link: '/changelog/' },
             { text: '命令帮助', link: '/commandhelp/' },
         ],
-        sidebar: 'auto',
-        lastUpdated: 'Last Updated',
+        sidebar: [
+            {
+              title: '首页',
+              collapsable: false,
+              sidebarDepth: 1,
+              children: [
+                '',
+                '/joinserver'
+              ]
+            }
+          ],
+        displayAllHeaders: true,
+        docsRepo: 'AerMoon-7/ServerHelp',
+        docsDir: 'docs',
+        docsBranch: 'main',
+        editLinks: true,
+        editLinkText: '在 GitHub 上编辑此页面',
+        lastUpdated: '上次更新',
     }
 }
